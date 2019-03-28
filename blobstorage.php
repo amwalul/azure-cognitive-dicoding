fa9c93d9b862d21fe96ec6ec1ced8e244624f646
Amwalul Ikhsani <43863694+amwalul@users.noreply.github.com>
2019-03-28 11:04:18 +0700

    Add files via upload

---
+++ blobstorage.php
@@ -0,0 +1,78 @@
+<?php
+require_once 'vendor/autoload.php';
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=dicodingweb;AccountKey=EniRF8FL5f7BHhYoJeRxPuST+fu9opnlxhh788ol1H4uybO520au54NV9uXfj1wC+N7reu7h7mbBZMqxza1rRg==;EndpointSuffix=core.windows.net";
+$containerName = "submission";
+// Create blob client.
+$blobClient = BlobRestProxy::createBlobService($connectionString);
+if (isset($_POST['submit'])) {
+	$fileToUpload = strtolower($_FILES["fileToUpload"]["name"]);
+	$content = fopen($_FILES["fileToUpload"]["tmp_name"], "r");
+	// echo fread($content, filesize($fileToUpload));
+	$blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+	header("Location: blobstorage.php");
+}
+$listBlobsOptions = new ListBlobsOptions();
+$listBlobsOptions->setPrefix("");
+$result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Analyze Image</title>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container mt-4">
+
+		<h1 class="text-center">Analyze Image</h1>
+		
+		<div class="mt-4 mb-2">
+			<form class="d-flex justify-content-center" action="blobstorage.php" method="post" enctype="multipart/form-data">
+				<input type="file" name="fileToUpload" accept=".jpeg,.jpg,.png" required="">
+				<input type="submit" name="submit" value="Upload">
+			</form>
+		</div>
+
+		<h2>Total Files : <?php echo sizeof($result->getBlobs())?></h2>
+		<table class='table table-hover'>
+			<thead>
+				<tr>
+					<th>File Name</th>
+					<th>File URL</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+				do {
+					foreach ($result->getBlobs() as $blob)
+					{
+						?>
+						<tr>
+							<td><?php echo $blob->getName() ?></td>
+							<td><?php echo $blob->getUrl() ?></td>
+							<td>
+								<form action="computervision.php" method="post">
+									<input type="hidden" name="url" value="<?php echo $blob->getUrl()?>">
+									<input type="submit" name="submit" value="Analyze!" class="btn btn-primary">
+								</form>
+							</td>
+						</tr>
+						<?php
+					}
+					$listBlobsOptions->setContinuationToken($result->getContinuationToken());
+				} while($result->getContinuationToken());
+				?>
+			</tbody>
+		</table>
+
+	</div>
+</body>
+</html>
